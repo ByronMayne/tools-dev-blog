c61ff193af1506ca9a095e49997d16694ea675ef
ByronMayne <byronmayne@gmail.com>
2022-02-09 21:42:39 -0500

    The project is mostly setup now

---
+++ docusaurus.config.js
@@ -0,0 +1,50 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Tools Dev Blog',
+  tagline: 'A series of blogs covering programing in various forms',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  githubHost: 'github.com',
+  deploymentBranch: 'gh-pages',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'Byron Mayne',
+  projectName: 'tools-dev-blog', 
+  themeConfig: {
+    hideableSidebar: true,
+    navbar: {
+      title: "Tools Dev Blog",
+      logo: {
+        alt: 'tools dev blog logo',
+        src: 'img/icon.png',
+        width: 32,
+        height: 32,
+      },
+      items: [
+        {
+          to: 'blog',
+          activeBasePath: 'docs',
+          label: 'Blog',
+          position: 'left',
+        },
+      ],
+    }
+  },
+  plugins: [
+    ['@docusaurus/theme-classic', {}],
+    ['@docusaurus/plugin-content-docs', {}],
+    ['@docusaurus/plugin-content-blog', {}],
+    ['@docusaurus/plugin-content-pages', {}],
+    ['@docusaurus/plugin-debug', {}],
+    ['@docusaurus/plugin-sitemap', {}],
+  ]
+};
+
+module.exports = config;
