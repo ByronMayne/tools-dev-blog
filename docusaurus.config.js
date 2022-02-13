5ed98f9f312b0bba47575ca92f078e891b42915a
ByronMayne <byronmayne@gmail.com>
2022-02-13 16:50:29 -0500

    Made broken links for now just a warning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   url: 'https://byronmayne.github.io',
   baseUrl: '/',
   deploymentBranch: 'gh-pages',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'ByronMayne',
